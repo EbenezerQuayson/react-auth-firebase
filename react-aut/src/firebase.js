e064c0b9d1bc4c395c53ad07d3219538d2a6298b
Ebenezer Quayson <2425401402@gctu.live.edu>
2025-12-09 19:20:12 +0000

    hiding api

--- react-aut/src/firebase.js
+++ react-aut/src/firebase.js
@@ -6,13 +6,14 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyALUBIYEgn6xK3DPF-Vk7rbe2a8m7pD3Ys",
-  authDomain: "react-auth-firebase-51966.firebaseapp.com",
-  projectId: "react-auth-firebase-51966",
-  storageBucket: "react-auth-firebase-51966.firebasestorage.app",
-  messagingSenderId: "929205831202",
-  appId: "1:929205831202:web:95fcaa880d594241296c7b"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
